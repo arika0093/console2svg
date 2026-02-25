9ad62c907e35d997745267e098a69107ab292c98
Arika Ishinami <delete0093@gmail.com>
2026-02-25 03:05:30 +0000

    Fix local-install.sh to ensure ConsoleToSvg is uninstalled before installation

--- local-install.sh
+++ local-install.sh
@@ -1,3 +1,4 @@
 export DOTNET_EnableWriteXorExecute=0
 dotnet pack -o publish
+dotnet tool uninstall -g ConsoleToSvg || true
 dotnet tool install -g  --add-source ./publish ConsoleToSvg --prerelease

548f7d48e04767b6fe9893957829abfe602f1015
Arika Ishinami <delete0093@gmail.com>
2026-02-26 07:12:45 +0000

    Fix: ensure publish directory is removed before packaging

--- local-install.sh
+++ local-install.sh
@@ -1,4 +1,5 @@
 export DOTNET_EnableWriteXorExecute=0
+rm -rf ./publish || true
 dotnet pack -o publish -p:WarningLevel=0
 dotnet tool uninstall -g ConsoleToSvg || true
 dotnet tool install -g  --add-source ./publish ConsoleToSvg --prerelease 

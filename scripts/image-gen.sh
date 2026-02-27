bd0112db08efe49cf88d939de505a771117316a7
Arika Ishinami <delete0093@gmail.com>
2026-02-27 05:16:47 +0000

    update images

--- scripts/image-gen.sh
+++ scripts/image-gen.sh
@@ -2,4 +2,4 @@
 console2svg -c -d macos-pc -w 100 -h 10 -o assets/cmd-hero.svg -- oh-my-logo "console2svg" mint --filled --letter-spacing 0
 console2svg console2svg -w 120 -o assets/cmd.svg 
 console2svg -w 120 -c -d macos-pc -o ./assets/cmd-window.svg -- console2svg
-console2svg -m video -c -d -o ./assets/cmd-loop.svg -- copilot --banner
+console2svg -v -c -d -h 28 -o ./assets/cmd-loop.svg -- copilot --banner

ce4ea96ec53db10716f12b7441989d963dbe5116
Arika Ishinami <delete0093@gmail.com>
2026-02-28 19:53:17 +0900

    miss fix

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -2,4 +2,4 @@ export DOTNET_EnableWriteXorExecute=0
 rm -rf ./publish || true
 dotnet pack -o publish -p:WarningLevel=0
 dotnet tool uninstall -g ConsoleToSvg || true
-dotnet tool install -g --prerelease 
+dotnet tool install -g ConsoleToSvg --prerelease 

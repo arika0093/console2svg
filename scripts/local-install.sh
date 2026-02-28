a8f4ad95c13823a1bfa76e4629b5bcae6a281625
Arika Ishinami <delete0093@gmail.com>
2026-02-28 19:52:12 +0900

    feat: add nuget.config for package source management and update local-install script

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -2,4 +2,4 @@ export DOTNET_EnableWriteXorExecute=0
 rm -rf ./publish || true
 dotnet pack -o publish -p:WarningLevel=0
 dotnet tool uninstall -g ConsoleToSvg || true
-dotnet tool install -g  --add-source ./publish ConsoleToSvg --prerelease 
+dotnet tool install -g --prerelease 

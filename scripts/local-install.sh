9fc3b9d6db7fd9dfe4bb22887615a0f0fa598c85
Arika Ishinami <delete0093@gmail.com>
2026-03-01 01:13:31 +0900

    Add clean and build steps to local installation script

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -1,5 +1,7 @@
 export DOTNET_EnableWriteXorExecute=0
 rm -rf ./publish || true
+dotnet clean
+dotnet build -c Release --no-cache
 dotnet pack -o publish -p:WarningLevel=0
 dotnet tool uninstall -g ConsoleToSvg || true
 dotnet tool install -g ConsoleToSvg --prerelease 
